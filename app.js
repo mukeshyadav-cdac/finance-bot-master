6fce1a7770307d3553ce14df694fc934a7258e39
Mukesh <ashu.3333d@gmail.com>
2017-06-08 10:46:46 +0530

    added post back to bot.

--- app.js
+++ app.js
@@ -2,57 +2,34 @@
 const express = require ('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const app = express ();
-const port = 5000;
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const config = require('./config');
-//var connection = connect();
 
-/**
- * Controllers (route handlers).
- */
-const userController = require('./controllers/user');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.MONGODB_URI);
 
-/**
- * Express configuration.
- */
+var server;
+const app = express ();
+
+app.set('port', (config.PORT || 7000))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use (express.static(__dirname + '/public'));
 
+const userController = require('./controllers/user');
 app.get('/about', userController.aboutView);
 app.get('/', userController.homeView);
 app.post('/', userController.getUserConnection);
 app.get('/transactions', userController.transactions);
 
-
-//set static files (css, images, etc) location
-app.use (express.static(__dirname + '/public'));
-
-// connection
-//   .on('error', console.log)
-//   .on('disconnected', connect)
-//   .once('open', listen);
-
-//start the server
-
-
-function listen () {
-  var server = app.listen (port, function () {
-    console.log('app started on port ' + port);
-  });
-
-  server.timeout = 6000000 ;
-}
-
-var server = app.listen (port, function () {
-  console.log('app started on port ' + port);
-});
-
-
-// function connect () {
-//   var connection = mongoose.connect(config.db).connection;
-//   return connection;
-// }
+mongoose.connection
+  .once('open', () => {
+    server = app.listen(app.get('port'), ()  =>  {
+      server.timeout = 3000000000;
+      console.log('running on port', app.get('port'))
+    });
+    console.log('Connected to MongoLab instance.')
+  })
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
